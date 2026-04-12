8b73896f14e45d7bebe94acdb41fc5f0215cd217
kavita2406 <kavita.mnnit@gmail.com>
2026-04-12 17:30:32 -0400

    C code for Leetcode Problem 605. Can Place Flowers
    Link to LeetCode Problem: https://leetcode.com/problems/can-place-flowers/description/?source=submission-noac

---
+++ plotFlower.c
@@ -0,0 +1,16 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    int count = 0;
+    for (int i = 0; i < flowerbedSize; i++) {
+        if (flowerbed[i] == 0) {
+            int emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+            int emptyRight =
+                (i == flowerbedSize - 1) || (flowerbed[i + 1] == 0);
+
+            if (emptyLeft && emptyRight) {
+                flowerbed[i] = 1; // plant flower
+                count++;
+            }
+        }
+    }
+    return count >= n;
+}
